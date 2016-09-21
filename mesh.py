8738de9ed8c6978c4e45d545aa4d20f8e497e2e3
denocris <denobili16@gmail.com>
2016-09-21 17:26:45 +0200

    creazione di mesh

--- mesh.py
+++ mesh.py
@@ -2,7 +2,51 @@ import numpy as np
 
 def read_msh(filename):
 
-    """ read mesh code """
+    x = np.array([])
+    y = np.array([])
+
+    nodes = np.array([])
+
+    b_nodes = np.array([])
+
+    topo = np.array([])
+
+    num_line = 0
+
+    f = open(filename,"r")
+
+    for line in f:
+        if line[0] == "$":
+            print "This is useless"
+        else:
+            l = map(float,line.split())
+            #print map(float,line.split())
+            if len(l) == 4:
+                x = np.append(x,l[1])
+                y = np.append(y,l[2])
+                nodes = np.append(nodes,l[0])
+
+            if len(l) == 8:
+                num_line += 1
+                topo = np.append(topo,l[5:8])
+
+            if len(l) == 7:
+                b_nodes = np.append(b_nodes,l[5:])
+
+
+    b_nodes_unique = np.array([])
+    for elem in b_nodes:
+        elem = elem - 1
+        if elem not in b_nodes_unique:
+            b_nodes_unique = np.append(b_nodes_unique,elem)
+
+    b_nodes = b_nodes_unique
+
+
+    topo = np.reshape(topo,(num_line,3))
+    topo = topo - 1
+    f.close()
+
 
     r_id = 0
     for row in topo:
